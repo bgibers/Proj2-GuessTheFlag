9518b5aca80abaa2f5714aa1f75a981f45cafabc
Brendan Giberson <giberson_brendan@bah.com>
2019-07-13 11:00:43 -0400

    Create base of project

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Proj2-GuessTheFlag
+//
+//  Created by 594168 on 7/13/19.
+//  Copyright © 2019 BrendanGiberson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
